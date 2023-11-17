3498db2672d69a6aed900bf4510d6fc665bef6ba
Collins Charles <31598209+el-collins@users.noreply.github.com>
2023-11-17 17:36:32 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,10 +11,10 @@ const Work = () => {
        {/* heading */}
         <div className="relative mt-20 lg:text-center">
           <h1 className="uppercase font-black font-primary text-[28px] lg:text-[60px] max-sm:bg-[#8b8b8b21] max-md:bg-none max-lg:bg-none py-4 px-4 mb-5">
-            Get in <span className="text-accent">touch</span>
+            My <span className="text-accent">Portfolio</span>
           </h1>
           <div className="font-primary absolute text-[80px] lg:text-[100px] lg:top-[15px] lg:left-[190px] text-[#ffffff14] uppercase font-black">
-          <span className="hidden lg:tracking-widest lg:block">Contact </span>
+          <span className="hidden lg:tracking-widest lg:block">Work </span>
             </div>
         </div>
 
