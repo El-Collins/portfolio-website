ed82e3c1d559e75c027ad9a9564d2701d06fe5ad
Collins Charles <31598209+el-collins@users.noreply.github.com>
2023-12-03 22:16:57 +0100

    Update projectdata.js

--- src/assets/projectdata.js
+++ src/assets/projectdata.js
@@ -13,7 +13,7 @@ const projectData = [
     description:
       "Movie world is a website for searching movies. Users can search their favorite movies, favorite superhero and supervillians movies.",
     code: "https://github.com/el-collins/Movie-world",
-    view: "",
+    view: "https://newmovieworld.vercel.app/",
   },
   {
     image: require("../assets/hangman.png"),
